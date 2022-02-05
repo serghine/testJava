445b62dc35627f1c1b89702146ac0c18f131cf67
serghine <sergh.boudj@gmail.com>
2022-02-05 23:12:12 +0100

    test projetJavaSpring

---
+++ src/main/java/com/example/test/TestApplication.java
@@ -0,0 +1,20 @@
+package com.example.test;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication implements CommandLineRunner  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	System.out.println("get message");
+		
+	}
+
+}
